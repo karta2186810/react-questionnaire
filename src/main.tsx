be3aff5c5b7eea464eae243d0146893fae24e914
karta2186810 <karta2186810@gmail.com>
2024-07-02 00:04:42 +0800

    chore: dev serve

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { IS_DEV } from './config';
 import { setupMockServer } from './mocks';
 import { AppProvider } from '@/providers';
 import App from './App';
@@ -8,7 +7,7 @@ import App from './App';
 bootstrap();
 
 async function bootstrap() {
-  if (IS_DEV) await setupMockServer();
+  await setupMockServer();
 
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
